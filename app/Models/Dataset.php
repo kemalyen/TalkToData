755d0c572396906a5d2ec9e261b85fd43ec3ddbd
kemal <kemalyen@gmail.com>
2026-08-24 15:11:02 +0100

    tests are added'

--- app/Models/Dataset.php
+++ app/Models/Dataset.php
@@ -2,21 +2,24 @@
 
 namespace App\Models;
 
+use Database\Factories\DatasetFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dataset extends Model
 {
-    /** @use HasFactory<\Database\Factories\DatasetFactory> */
+    /** @use HasFactory<DatasetFactory> */
     use HasFactory;
 
-    protected $fillable = ['name', 'file_path', 'schema_json', 'row_count'];
+    protected $fillable = ['user_id', 'name', 'file_path', 'schema_json', 'row_count'];
 
     protected $casts = ['schema_json' => 'array'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function conversations()
     {
         return $this->hasMany(Conversation::class);
